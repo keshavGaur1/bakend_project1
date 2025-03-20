93899f8ccf7d0c004ac5e63a84a95f46700ce63f
Keshav Gaur <keshavgaur28@gmail.com>
2025-03-20 21:08:56 +0530

    all setup config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+// cloudinary per file - file system ke through aayegi ( matlab hmare server se )
+
+// server -> cloudinary 
+
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if( !localFilePath ){
+            return null ;
+        }
+
+        // uploading file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+            // koi bhi format aye khud detect kr lega
+        })
+        // file has been uploaded successfully
+        console.log('File uploaded successfully on cloudinary !', response.url);
+
+        return response;
+    } catch (error){
+        // remove locally stored temp. file from server  
+        fs.unlinkSync(localFilePath);
+        console.error('Error in uploading file on cloudinary !', error);
+        return null;
+    }
+}
+
+export { uploadOnCloudinary };
